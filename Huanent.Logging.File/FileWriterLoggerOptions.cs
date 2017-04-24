137bf60b9b6a693ac9c1a367e4750fc0fbbb81f4
huanent <huanent@hotmail.com>
2017-04-24 10:54:24 +0800

    firstCommit

---
+++ Huanent.Logging.File/FileWriterLoggerOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace Logging.TextWriter
+{
+    public class FileWriterLoggerOptions : IOptions<FileWriterLoggerOptions>
+    {
+        /// <summary>
+        /// 日志所在文件夹绝对路径
+        /// </summary>
+        public string BasePath { get; set; } = AppContext.BaseDirectory;
+
+        /// <summary>
+        /// 日志记录级别
+        /// </summary>
+        public LogLevel LogLevel { get; set; } = LogLevel.Warning;
+
+        public FileWriterLoggerOptions Value => this;
+    }
+}
---
+++ Huanent.Logging.File/FileWriterTraceListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Logging.TextWriter
+{
+    class FileWriterTraceListener : TextWriterTraceListener
+    {
+        string _basePath;
+
+        /// <summary>
+        /// 文件日志写入器
+        /// </summary>
+        /// <param name="basePath">文件存放根目录</param>
+        public FileWriterTraceListener(string basePath)
+        {
+            _basePath = basePath;
+        }
+
+        public override void Write(string message)
+        {
+            var msgBuilder = new StringBuilder();
+            msgBuilder.Append("-----");
+            msgBuilder.Append(DateTime.Now.ToString("HH:mm:ss"));
+            msgBuilder.AppendLine("-------------------------------------");
+            msgBuilder.Append(message);
+            File.AppendAllText($"{_basePath}\\{DateTime.Now.Date.ToString("yyyMMdd")}.txt", msgBuilder.ToString());
+        }
+
+        public override void WriteLine(string message)
+        {
+            var msgBuilder = new StringBuilder();
+            msgBuilder.AppendLine(message);
+            msgBuilder.AppendLine();
+            File.AppendAllText($"{_basePath}\\{DateTime.Today.ToString("yyyMMdd")}.txt", msgBuilder.ToString());
+        }
+    }
+}
